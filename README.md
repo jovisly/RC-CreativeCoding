316d4aed46534e99210722e617efb88c85b19af8
jovisly <j@ovisly.com>
2023-11-08 10:28:56 -0800

    set up

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# one-dot
+# One Dot
+
+A little app made during Creative Coding on Nov. 8, 2023, which is also in
+preparation for a React Native session the following week.
+
+## Set Up
+
+This app is built with [Expo](https://expo.dev/).
+
+```
+> npx create-expo-app app
+```
+
+The command will create an `app` directory.
+
+## Running the App Locally
+
+You can open the app in multiple ways. If you have iOS simulator installed, you
+can do `npm run ios`. Since Expo projects support web (via `react-native-web`),
+you can also do `npm run web` to have a local web server.
---
+++ app/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>You can only make one dot at a time!</Text>
+      <Button
+        style={{ backgroundColor: "pink", width: 100, height: 100 }}
+        title="Press me"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
