ac36ffa1ce7cda7a87a6bf5d21f62be9b9678787
jovisly <j@ovisly.com>
2023-11-08 10:44:17 -0800

    refactor

--- app/App.js
+++ app/App.js
@@ -1,29 +1,7 @@
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 
-import Button from "./components/Button";
-
-const RenderIntro = ({ setIsIntro }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 18 }}>You can only make one dot at a time!</Text>
-      <View style={{ height: 18 }} />
-      <Button
-        title="OK!"
-        onPress={() => {
-          setIsIntro(false);
-        }}
-        iconName="thumb-up"
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-const RenderGame = () => {
-  return null;
-};
+import RenderIntro from "./components/RenderIntro";
+import RenderGame from "./components/RenderGame";
 
 export default function App() {
   const [isIntro, setIsIntro] = useState(true);
@@ -33,12 +11,3 @@ export default function App() {
     return <RenderGame />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app/components/RenderGame.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const RenderGame = () => {
+  // What elements will be here?
+  return null;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default RenderGame;
