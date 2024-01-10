95f02d2c76227a57de277964bc2dc64fef92fd5a
jovisly <j@ovisly.com>
2024-01-10 15:02:21 -0800

    jack attack!

--- murphys-law-tetris/app/src/App.jsx
+++ murphys-law-tetris/app/src/App.jsx
@@ -1,61 +1,129 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tetris from "react-tetris";
 
 import "./App.css";
 import DisableScroll from "./DisableScroll";
+import jackImg from "./assets/jack-attack-small.png";
+import upbeatMusic from "./assets/upbeat.mp3";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  const [hasStarted, setHasStarted] = useState(false);
+
+  useEffect(() => {
+    const audio = new Audio(upbeatMusic);
+    if (hasStarted) {
+      audio.play();
+    }
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, [hasStarted]);
+
+  useEffect(() => {
+    if (isVisible) {
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 5_000);
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    if (!hasStarted) {
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setIsVisible(true);
+
+      const interval = setInterval(() => {
+        setIsVisible(true);
+      }, 45_000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }, 45_000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [hasStarted]);
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100vw",
+        height: "90vh",
+      }}
+    >
       <h1>{"Murphy's Law Tetris"}</h1>
-      <Tetris
-        keyboardControls={{
-          down: "MOVE_DOWN",
-          left: "MOVE_LEFT",
-          right: "MOVE_RIGHT",
-          space: "HARD_DROP",
-          z: "FLIP_COUNTERCLOCKWISE",
-          x: "FLIP_CLOCKWISE",
-          up: "FLIP_CLOCKWISE",
-          p: "TOGGLE_PAUSE",
-          c: "HOLD",
-          shift: "HOLD",
-        }}
-      >
-        {({ Gameboard, points, level, state, controller }) => (
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            <Gameboard />
+      {hasStarted == false && (
+        <button
+          onClick={() => {
+            setHasStarted(true);
+          }}
+        >
+          Start
+        </button>
+      )}
+      {hasStarted && (
+        <Tetris
+          keyboardControls={{
+            down: "MOVE_DOWN",
+            left: "MOVE_LEFT",
+            right: "MOVE_RIGHT",
+            space: "HARD_DROP",
+            z: "FLIP_COUNTERCLOCKWISE",
+            x: "FLIP_CLOCKWISE",
+            up: "FLIP_CLOCKWISE",
+            p: "TOGGLE_PAUSE",
+            c: "HOLD",
+            shift: "HOLD",
+          }}
+        >
+          {({ Gameboard, points, level, state, controller }) => (
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <Gameboard />
 
-            <div style={{ width: "24px" }} />
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-              }}
-            >
-              <div>
-                <h3>Points: {points}</h3>
-              </div>
-
-              <div>
-                <h3>Level: {level}</h3>
-              </div>
-
-              <div style={{ height: "24px" }} />
-
-              {state === "LOST" && (
+              <div style={{ width: "24px" }} />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  width: "180px",
+                }}
+              >
                 <div>
-                  <h2>Game Over</h2>
-                  <button onClick={controller.restart}>New game</button>
+                  <h3>Points: {points}</h3>
                 </div>
-              )}
+
+                <div>
+                  <h3>Level: {level}</h3>
+                </div>
+
+                <div style={{ height: "24px" }} />
+
+                {state === "LOST" && (
+                  <div>
+                    <h2>Game Over</h2>
+                    <button onClick={controller.restart}>New game</button>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-      </Tetris>
+          )}
+        </Tetris>
+      )}
+      {isVisible && (
+        <img src={jackImg} alt="JackAttack" className="imageJack" />
+      )}
       <DisableScroll />
     </div>
   );
